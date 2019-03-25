0a5bea888b6e6e24f242f57926e402fff5c19a8c
Febriansyah <fbriansyah@gmail.com>
2019-03-25 11:33:47 +0700

    Styling Auth Screen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { Navigation } from 'react-native-navigation'
 import { Provider } from 'react-redux'
 
+// Screens
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
-import configureStore from './src/store/configureStore';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 
+// Redux Setup
+import configureStore from './src/store/configureStore';
 const store = configureStore()
 
 // Register screens
--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { Component } from 'react';
+import { View, ImageBackground, StyleSheet } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from './../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from './../../components/UI/HeadingText/HeadingText';
+import MainText from './../../components/UI/MainText/MainText';
 
-export default class AuthScreen extends Component {
+import backgroundImage from '../../assets/adventure-alps-background.jpg';
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
+
+class AuthScreen extends Component {
 
   loginHandler = () => {
     startMainTabs()
@@ -10,10 +16,40 @@ export default class AuthScreen extends Component {
 
   render () {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler} />
-      </View>
+      <ImageBackground source={backgroundImage} style={styles.ImageBackground}>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Please Log in</HeadingText>
+          </MainText>
+          <ButtonWithBackground color="#29aaf4" onPress={ () => alert("Login") }>Switch to Login</ButtonWithBackground>
+          <View style={styles.inputContainer}>
+            <DefaultInput placeholder="Your E-Mail Address" style={styles.input} />
+            <DefaultInput placeholder="Password" style={styles.input} />
+            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+          </View>
+          <ButtonWithBackground onPress={this.loginHandler} color="#55c57a">Submit</ButtonWithBackground>
+        </View>
+      </ImageBackground>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    backgroundColor: "#eee", 
+    borderColor: "#bbb"
+  },
+  ImageBackground: {
+    flex: 1
+  }
+})
+
+export default AuthScreen
